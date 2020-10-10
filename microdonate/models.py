4f21cf74efc8b8edc8eb974a6d58ef1d6b2f1899
John Kothmann <kothmannj@gmail.com>
2020-10-10 15:35:22 -0400

    Added login functionality with "Profile" model

--- microdonate/models.py
+++ microdonate/models.py
@@ -6,12 +6,18 @@ from django.contrib.auth.models import User
 
 # TODO: Create your models here.
 
+class Profile(models.Model):
+    account = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    # check User.name
+    user_name = models.CharField(max_length=25, null=True)
+    xp = models.IntegerField(default=0)
+
+
 class Donate(models.Model):
     donate_name = models.CharField(max_length=200)
 
     donate_user = models.ForeignKey(
-        'Profile',
-        on_delete=models.CASCADE,
+        Profile, on_delete = models.CASCADE
     )
     # ...
 
@@ -19,15 +25,8 @@ class Volunteer(models.Model):
     volunteer_name = models.CharField(max_length=200)
 
     volunteer_user = models.ForeignKey(
-        'Profile',
-        on_delete=models.CASCADE,
+        Profile, on_delete = models.CASCADE
     )
     # ...
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    # check User.name
-    user_name = models.CharField(User.username, null=True, on_delete=models.CASCADE)
-    xp = models.IntegerField(default=0)
-    opportunities = models.OneToOneField(Donate.donate_name, null=True, on_delete=models.CASCADE)
