8855243d24cfaf9515efed0304eec06d9b5cd4e4
Justin <jyg2qhc@virginia.edu>
2020-10-19 11:17:36 -0400

    fix tests

--- microdonate/tests.py
+++ microdonate/tests.py
@@ -12,13 +12,13 @@ from .models import Profile, Volunteer, Donate
 class TravisTesting(TestCase):
 
     # check if travis works
-    def always_passing_test(self):
+    def test_always_passing_test(self):
         self.assertTrue(True)
-    def one_plus_one(self):
+    def test_one_plus_one(self):
         self.assertEqual(2,1+1)
 
 class ProgressBarTesting(TestCase):
-    def percentage_accurate(self):
+    def test_percentage_accurate(self):
         prof = Profile.objects.create(account=None, user_name="me", xp=450)
         levels = [0,1000,2000,3500,5000,6750,8500,10000]
         for idx,exp in enumerate(levels):
@@ -32,32 +32,36 @@ class ProgressBarTesting(TestCase):
             needed = levels[level+1] - prof.xp
         self.assertEqual(percentage, 45)
             
-    def level_accurate(self):
+    def test_level_accurate(self):
         prof = Profile.objects.create(account=None, user_name="me", xp=4500) #level 3
+        levels = [0,1000,2000,3500,5000,6750,8500,10000]
         level = 0
         for idx,exp in enumerate(levels):
             if prof.xp >= exp:
                 level = idx
         self.assertEqual(level,3)
     
-    def level_case(self):
+    def test_level_case(self):
         prof = Profile.objects.create(account=None, user_name="me", xp=10000) #level 7 (maxed)
+        levels = [0,1000,2000,3500,5000,6750,8500,10000]
         level = 0
         for idx,exp in enumerate(levels):
             if prof.xp >= exp:
                 level = idx
         self.assertEqual(level,7)
 
-    def level_edge_case(self):
+    def test_level_edge_case(self):
         prof = Profile.objects.create(account=None, user_name="me", xp=12000) #level 7 (maxed)
+        levels = [0,1000,2000,3500,5000,6750,8500,10000]
         level = 0
         for idx,exp in enumerate(levels):
             if prof.xp >= exp:
                 level = idx
         self.assertEqual(level,7)
     
-    def level_edge_case2(self):
+    def test_level_edge_case2(self):
         prof = Profile.objects.create(account=None, user_name="me", xp=0) #level 0
+        levels = [0,1000,2000,3500,5000,6750,8500,10000]
         level = 0
         for idx,exp in enumerate(levels):
             if prof.xp >= exp:
@@ -66,11 +70,11 @@ class ProgressBarTesting(TestCase):
 
         
 class ProfileTesting(TestCase):
-    def new_profile_username(self):
+    def test_new_profile_username(self):
         prof = Profile.objects.create(account=None, user_name="new profile", xp=0)
         self.assertEqual("new profile", prof.user_name)
 
 class VolunteerTesting(TestCase):
-    def new_volunteer(self):
-        v = Volunteer.objects.create(volunteer_name="test_name",volunteer_users=None)
+    def test_new_volunteer(self):
+        v = Volunteer.objects.create(volunteer_name="test_name")
         self.assertEqual("test_name",v.volunteer_name)
