97de00eaeb6316c764980eb9112376069e953279
John Kothmann <kothmannj@gmail.com>
2020-10-19 03:06:27 -0400

    Good old syntactic issues

--- microdonate/models.py
+++ microdonate/models.py
@@ -16,13 +16,13 @@ class Profile(models.Model):
 class Donate(models.Model):
     donate_name = models.CharField(max_length=200)
 
-    donate_users = models.ManytoManyField(Profile)
+    donate_users = models.ManyToManyField(Profile)
     # ...
 
 class Volunteer(models.Model):
     volunteer_name = models.CharField(max_length=200)
 
-    volunteer_users = models.ManytoManyField(Profile)
+    volunteer_users = models.ManyToManyField(Profile)
     # ...
 
 
